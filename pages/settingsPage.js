511c864bb4a55b33665e3dab8ef9e95bc1b43de7
Maja Jensen <eaamnje@students.eaaa.dk>
2020-02-21 14:49:11 +0100

    Function slettet

--- pages/settingsPage.js
+++ pages/settingsPage.js
@@ -1,7 +1,7 @@
 export default class SettingsPage {
   constructor() {
     this.template();
-		this.slider();
+    this.slider();
   }
 
   template() {
@@ -31,15 +31,15 @@ export default class SettingsPage {
         </section>`
   }
 
-slider() {
-	let slider = document.getElementById("myRange");
-	let output = document.getElementById("demo");
-	output.innerHTML = slider.value;
+  slider() {
+    let slider = document.getElementById("myRange");
+    let output = document.getElementById("demo");
+    output.innerHTML = slider.value;
 
-	slider.oninput = function() {
-		output.innerHTML = this.value;
-	}
-}
+    slider.oninput = function () {
+      output.innerHTML = this.value;
+    }
+  }
 
   myFunction() {
     var x = document.getElementById("myText").value;
@@ -58,9 +58,10 @@ slider() {
 
   }
 
-  function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
