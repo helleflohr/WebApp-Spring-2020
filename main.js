459caa690aabcb550c66876bb4a1115d1b94b7bd
Maja Jensen <eaamnje@students.eaaa.dk>
2020-02-21 10:12:28 +0100

    Lave flere sider

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 import LoginPage from "./pages/loginPage.js"
 import AddPredefinedPage from "./pages/addPredefinedPage.js"
 import HomeWarningPage from "./pages/homeWarning.js"
+import CreatePartyPage from "./pages/createPartyPage.js"
 
 
 // Importing services
 import spaService from "./services/spa.js"
 import _categoryService from "./services/categoryService.js"
 import authService from "./services/loginService.js";
+import CreatePartyPage from "./pages/createPartyPage.js"
 
 
 
@@ -16,6 +18,7 @@ import authService from "./services/loginService.js";
 let loginPage = new LoginPage();
 let addPredefinedPage = new AddPredefinedPage();
 let homeWarning = new HomeWarningPage();
+let createPartyPage = new CreatePartyPage();
 
 
 // Initiating services
@@ -26,12 +29,14 @@ spaService.init();
 
 
 _categoryService.read();
-
-
 loginPage.template();
+
+
 homeWarning.template();
 addPredefinedPage.template();
+createPartyPage.template();
 
 // onclick handlers
 window.pageChange = () => spaService.pageChange();
 window.createQuestion = () => addPredefinedPage.createQuestion();
+window.checkedCategory = (id) => addPredefinedPage.checkedOrNot(id)
--- pages/homeWarning.js
+++ pages/homeWarning.js
@@ -8,8 +8,10 @@ export default class HomeWarningPage {
 
 
         document.querySelector('#content').innerHTML += /*html*/ `
-        <article id="home" class="page">
-        <a href="#addPredefinded">Drik ansvarligt gutter</a>
+       <article id="home" class="page">
+        <a href="#createPartyPage">
+        <p>Drik ansvarligt gutter</p>
+        <p>I drikker på eget ansvar, det skal vi ikke have noget med at gøre da!</p></a>
         </article>
         `;
 
